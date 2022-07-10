5d3d210871f19b6f0a77786cf82a59dd7db62439
Yatharth Ahuja <39669161+yatharthahuja@users.noreply.github.com>
2022-07-10 22:58:34 +0530

    Update polar_shift.m

--- polar_shift.m
+++ polar_shift.m
@@ -4,9 +4,16 @@ function [az_,elev_,r_] = polar_shift(az,elev,r,a,b,c,psi,theta,phi)
 %   Outputs: new polar coordinates
 %   works by converting into intermediate cartesian coordinates
 
+printf("First Translation then Rotation:\n")
 [x, y, z] = sph2cart(az,elev,r);
 [x, y, z] = trans_shift_cart(x,y,z,a,b,c);
 [x, y, z] = rot_shift_cart(x,y,z,psi,theta,phi);
+[az_,elev_,r_] = cart2sph(x,y,z)
+
+printf("First Rotation then Translation:\n")
+[x, y, z] = sph2cart(az,elev,r);
+[x, y, z] = rot_shift_cart(x,y,z,psi,theta,phi);
+[x, y, z] = trans_shift_cart(x,y,z,a,b,c);
 [az_,elev_,r_] = cart2sph(x,y,z)
 
 end
